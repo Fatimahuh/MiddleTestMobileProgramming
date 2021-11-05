1b5ab7e733cec84ed9cfb27c41791e30af43d1d9
fatimah.uswatun <fatimah.hasanah@wirecard.com>
2021-11-06 03:05:19 +0700

    UTS DONE

---
+++ AplikasiDasarMidleTestMobileProgramming/app/src/main/java/com/androidaplikasidasarmidletestmobileprogramming/MainActivity.java
@@ -0,0 +1,29 @@
+package com.androidaplikasidasarmidletestmobileprogramming;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity {
+private int STORAGE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+            }
+        }
+
